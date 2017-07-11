fa5a003cb18dd452de9e6d8d287b6de18e2c3a4f
HebleV <visheble@yahoo.com>
2017-07-12 00:17:16 +0530

    Added Networth, Weight and P/E Ratio functionalities

--- script.js
+++ script.js
@@ -2,56 +2,109 @@
 
  app.controller('stockCtrl', function($scope, $http) {
 
-
-    //**************** Begin of json data access**********************
+     var price = [];
+     var stocknum = [];
+     //**************** Begin of json data access**********************
      $http.get('data.json').then(function(response) {
          $scope.stocksArray = [];
-         $scope.weight = [];
          var indexes = [];
+         // var epsindex = [];
+
+
+
          $scope.stocks = response.data.price;
-    //**************** End of json data access**********************
+         $scope.eps = response.data.eps;
+         // console.log(eps);
 
 
-    //**************** Begin of adding data to table **********************
-         $scope.addToTable = function(key, value, index) {
+         //**************** Begin of adding data to table **********************
+         $scope.addToTable = function(key, value, index, shares = 1) {
              var mystock = {};
              mystock.key = key;
              mystock.value = value;
              mystock.index = index;
+             mystock.shares = shares;
+             mystock.eps = $scope.eps[key];
+             mystock.individualStock = function() {
+                 return this.value * this.shares;
+             };
              if (indexes.indexOf(index) == -1) {
                  indexes.push(index);
+                 //   console.log(mystock.shares);
+                 price.push(value);
                  $scope.stocksArray.push(mystock);
-                 $scope.weight = mystock.value;
+                 // for (var i=0; i<stocknum.length;i++) {
+                 //    console.log (stocknum.length);
+                 // }
 
              }
+
          };
-    //**************** End of adding data to table*************************
+         //****************** Begin of Incrementer and Decrementer*****************
+         $scope.incrementValue = function(stock) {
+             $scope.stocksArray[$scope.stocksArray.indexOf(stock)].shares++;
 
 
-    //****************** Begin of Incrementer and Decrementer*****************
-         $scope.incrementValue = function(index) {
-            number =1;
-             var value = parseInt(document.getElementById('number' + index).value, 10);
-             value = isNaN(value) ? 1 : value;
-             value++;
-             document.getElementById('number' + index).value = value;
          };
 
-         $scope.decrementValue = function(index) {
-            number =1;
-             value = parseInt(document.getElementById('number' + index).value, 10);
-             value = isNaN(value) ? 1 : value;
-             value--;
-             document.getElementById('number' + index).value = value;
+         $scope.decrementValue = function(stock) {
+             if (stock.shares < 1) {
+                 stock = 0;
+                 display(stock);
+             } else {
+                 $scope.stocksArray[$scope.stocksArray.indexOf(stock)].shares--;
+             }
          };
 
      });
 
-    //****************** End of Incrementer and Decrementer*****************
+     //****************** End of Incrementer and Decrementer***********************
+     // mystock.peratio = function() {
+     //          return this.value*this.shares;
+     //       };  
+     //          if ((epsindex.indexof(index)== -1)) {
+     //              epsindex.push(index);
+     //              console.log(epsindex);
+     //          }
+
+     //**************** End of adding data to table*************************
+
+     $scope.getNetWorth = function() {
+         var total = 0;
+
+         for (var i = 0; i < $scope.stocksArray.length; i++) {
+             total += $scope.stocksArray[i].individualStock();
+             // console.log();
+         }
+
+         return total.toFixed(2);
+     };
+
+     $scope.getPortfolioPE = function() {
+         var temp = 0;
+         var total = 0;
+         for (var i = 0; i < $scope.stocksArray.length; i++) {
+             total += $scope.stocksArray[i].eps * $scope.stocksArray[i].shares;
+         }
+         if (total === 0) {
+             total = 1;
+         }
+         temp += $scope.getNetWorth() / total;
+         return temp.toFixed(2);
+
+     };
+
+
+     $scope.remove = function(idx) {
+         $scope.stocksArray.splice(idx, 1);
+     };
+
+
  });
 
 
-//****************************** Begin of Filter*************************
+
+ //****************************** Begin of Filter*************************
 
  app.filter('objLimitTo', [function() {
      return function(obj, limit) {
@@ -74,4 +127,3 @@
          return result;
      };
  }]);
-//****************** End of Filter*****************
